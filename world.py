0425eab6302f751334ce54e3f0be3ec7aeecb249
PurwantoGz <purwanto@outlook.com>
2016-06-13 20:13:09 +0700

    Commit Pertama

---
+++ world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def changeLisk(listku):
+	listku.append(3);
+	print 'values inside function',listku;
+	return;
+mylisk=[20,23,230];
+changeLisk(mylisk);
+print mylisk;
+print "Good Bye";
